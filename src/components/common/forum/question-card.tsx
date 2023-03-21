07597ea81ce17745979b5151b9071e7774ec16af
unknown <shapu953@gmail.com>
2023-03-21 23:09:41 +0530

    modify: forum

--- src/components/common/forum/question-card.tsx
+++ src/components/common/forum/question-card.tsx
@@ -1,63 +1,59 @@
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import { Image, Text, TouchableOpacity, View } from 'react-native';
+import { useGetQuestion } from '../../../hooks/questions/use-get-question';
 import useRouter from '../../../hooks/router/use-router';
 
 dayjs.extend(relativeTime);
 
 type Props = {
-  title: string;
-  description: string;
-  date: any;
-  votes: any;
   id: string;
-  author_firstname: string;
-  author_lastname: string;
-  author_id: string;
-  answers: any;
 };
 
-const QuestionCard = ({
-  title,
-  description,
-  date,
-  votes,
-  id,
-  author_firstname,
-  answers,
-  author_id,
-  author_lastname,
-}: Props) => {
+const QuestionCard = ({ id }: Props) => {
   const router = useRouter('All Questions');
+  const { question, loading, error } = useGetQuestion(id);
+
+  if (loading) {
+    return (
+      <View className='flex justify-center items-center min-h-screen'>
+        <Text className='font-main font-bold text-xl text-primary-main'>
+          Hold on. The question is loading...
+        </Text>
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View className='flex justify-center items-center min-h-screen'>
+        <Text className='font-main font-bold text-xl text-red'>
+          {`Seems like there is an error: ${error}`}
+        </Text>
+      </View>
+    );
+  }
 
   return (
     <TouchableOpacity
-      className='mb-2 py-5 px-4 flex flex-row justify-around items-center bg-primary-main rounded-lg'
+      className='mb-2 py-5 px-4 flex flex-row justify-around items-center bg-primary-light rounded-lg'
       onPress={() =>
         router.push('Single Question', {
           id: id,
-          title: title,
-          question: description,
-          date: date,
-          votes: votes,
-          answers: answers,
-          author_firstname: author_firstname,
-          author_lastname: author_lastname,
-          author_id: author_id,
         })
       }
     >
-      <View className='pl-1 pr-6 flex items-center'>
+      <View className='pr-6 flex items-end'>
         <View>
           <Image source={require('../../../assets/images/like.png')} className='h-4 w-4' />
         </View>
 
-        <Text className='font-bold font-main text-xs pt-1'>{votes.length}</Text>
+        <Text className='font-bold font-main text-xs pt-1'>{question?.votes.length}</Text>
       </View>
-      <Text className='font-bold font-main w-3/4 px-2'>{title}</Text>
-      {date && (
+      <Text className='font-bold font-main w-3/4 px-2'>{question?.title}</Text>
+      {question?.created && (
         <Text className='font-bold font-main text-xs'>
-          {dayjs(date.toDate()).fromNow(true)} ago
+          {dayjs(question.created.toDate()).fromNow(true)} ago
         </Text>
       )}
     </TouchableOpacity>
--- src/screens/all-questions-screen.tsx
+++ src/screens/all-questions-screen.tsx
@@ -11,6 +11,26 @@ const AllQuestionsScreen = (props: Props) => {
   const { questions, loading, error } = useAllQuestions();
   const backgroundStyle = 'bg-white dark:bg-slate-900 h-screen';
 
+  if (loading) {
+    return (
+      <View className='flex justify-center items-center min-h-screen'>
+        <Text className='font-main font-bold text-xl text-primary-main'>
+          Hold on. The forum is loading...
+        </Text>
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View className='flex justify-center items-center min-h-screen'>
+        <Text className='font-main font-bold text-xl text-red'>
+          {`Seems like there is an error: ${error}`}
+        </Text>
+      </View>
+    );
+  }
+
   return (
     <SafeAreaView className=' bg-white'>
       <View className='p-4 h-full relative'>
@@ -27,17 +47,7 @@ const AllQuestionsScreen = (props: Props) => {
               )
               .map((question) => (
                 <View key={question.id}>
-                  <QuestionCard
-                    id={question.id}
-                    title={question.title}
-                    description={question.question}
-                    date={question.created}
-                    votes={question.votes}
-                    answers={question.answers}
-                    author_firstname={question.author.first_name ? question.author.first_name : ''}
-                    author_lastname={question.author.last_name ? question.author.last_name : ''}
-                    author_id={question.author.id ? question.author.id : ''}
-                  />
+                  <QuestionCard id={question.id} />
                 </View>
               ))
           ) : (
@@ -50,7 +60,7 @@ const AllQuestionsScreen = (props: Props) => {
         </ScrollView>
         <View className='absolute bottom-0 right-0 mx-3 my-3 z-20'>
           <TouchableOpacity
-            className='bg-primary-main p-4 rounded-full'
+            className='bg-primary-light p-4 rounded-full'
             onPress={() => router.navigate('Add Question')}
           >
             <Image className='h-6 w-6 p-3' source={require('../assets/images/plus.png')} />
